9ebe6c136f01a829ece5d6933fbf74069b665e3b
Luis Felipe Covelo <covelo.luis@hotmail.com>
2019-03-05 18:18:16 -0300

    Modificando o escopo do projeto para o padrão MVC

--- client/index.js
+++ client/index.js
@@ -1,33 +1,39 @@
+//Criando a lista do tipo array com os campos disponíveis no formulario
 var campos = [
 	document.querySelector("#data"),
 	document.querySelector("#quantidade"),
 	document.querySelector("#valor")
 ];
 
+//Pego o valor do tbody
 var tbody = document.querySelector("table tbody");
 
-document.querySelector(".form").addEventListener("submit", function(event) {
+//Acrescento uma função submit ao formulario
+document.querySelector('.form').addEventListener('submit', function(event){
 	
 	event.preventDefault();
-	
+
+	//Crio uma coluna para vincular as tds abaixo
 	var tr = document.createElement("tr");
 
+	//Faço um foreach dentro de campos para listar
 	campos.forEach(function(campo){
 		var td = document.createElement("td");
 		td.textContent = campo.value;
 		tr.appendChild(td);
-	});
-
+	})
+	
 	var tdVolume = document.createElement("td");
 	tdVolume.textContent = campos[1].value * campos[2].value;
-
+	
 	tr.appendChild(tdVolume);
 	tbody.appendChild(tr);
-	
-	campos[0].value = '';
+
+	campos[0].value = "";
 	campos[1].value = 1;
-	campos[2].value = 0.0;
+	campos[2].value = 0;
 
 	campos[0].focus();
 
 });
+
